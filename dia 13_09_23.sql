19f3011f0b566861f8d9ecc6f268ace9f2771cd0
Argoze <118485283+Argoze@users.noreply.github.com>
2023-09-13 10:37:24 -0300

    dia 13_09_23.sql

--- dia 13_09_23.sql
+++ dia 13_09_23.sql
@@ -14,7 +14,7 @@ select MAX(Valor) Maximo, MIN(Valor) Minimo from veiculo --vendo o maximo de val
 select distinct AnoFabricacao from Veiculo
 select AnoFabricacao, Count(*) Qtd from Veiculo group by AnoFabricacao -- vendo quanto carro tem em cada ano
 
-SELECT AnoFabricacao, SUM(Valor) SomaValorVeiculos FROM Veiculo -- vendo os anos de fabricaçao e a soma dos valores 
+SELECT AnoFabricacao, SUM(Valor) SomaValorVeiculos FROM Veiculo -- vendo os anos de fabricaÃ§ao e a soma dos valores 
 Group by AnoFabricacao -- agrpando tudo de cima em um so 
 HAVING SUM(Valor) >= 50000 --delimitando um valor para que mostre valores menor ou igual a 50000
 
